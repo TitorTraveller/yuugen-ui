fe0e84a1256fa0d2cc8a4b95e4c8916271a90a5f
Francisco Poblete <frpobleteberrios@gmail.com>
2025-02-17 16:34:27 -0300

    Removed vanilla extract deprecated files imports

--- lib/components/Button/Button.tsx
+++ lib/components/Button/Button.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { button } from "./Button.css";
 
 interface ButtonProps {
 	label: string;
--- lib/components/Card/Card.tsx
+++ lib/components/Card/Card.tsx
@@ -1,4 +1,3 @@
-import { card } from "./Card.css";
 const Card = ({ children }: { children: React.ReactNode }) => <div className={card}>{children}</div>;
 
 export default Card;
